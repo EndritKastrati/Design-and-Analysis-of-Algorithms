6a0d9de8dd54a5757096882ededc1662f15f48c1
Endrit Kastrati <endritkastrati8@gmail.com>
2024-11-13 00:06:09 +0100

    Adding Bubble Sort Algorithm

---
+++ sorting_algorithms/bubble_sort.js
@@ -0,0 +1,24 @@
+function bubbleSort(arr, n){
+
+    let temp, ndrruar;
+
+    for(let i=0; i<n-1; i++){
+
+        ndrruar = false;
+        for(let j=0; j<n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                ndrruar = true;
+            }
+        }
+        if(ndrruar === false){
+            break;
+        }
+    }
+    return arr;
+}
+
+
+console.log(bubbleSort([8, 4, 5, 1, 9, 7, 6, 10, 3 , 2], 10));
