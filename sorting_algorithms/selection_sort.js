c7bac65fdbca0f191d2429a386faed3143d37b54
Endrit Kastrati <endritkastrati8@gmail.com>
2024-11-13 00:00:36 +0100

    Adding Selection Sort Algorithm

---
+++ sorting_algorithms/selection_sort.js
@@ -0,0 +1,21 @@
+function selectionSort(vargu){
+
+    let n = vargu.length;
+
+    for(let i=0; i<n-1; i++){
+
+        let min_index = i;
+        for(let j=i+1; j<n; j++){
+            if(vargu[j] < vargu[min_index]){
+                min_index = j;
+            }
+        }
+
+        let temp = vargu[i];
+        vargu[i] = vargu[min_index];
+        vargu[min_index] = temp;
+    }
+    return vargu;
+}
+
+console.log(selectionSort([8, 4, 5, 1, 9, 7, 6, 10, 3 , 2]));
